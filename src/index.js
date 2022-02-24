119812469306465207801c10273369e46011462a
Alyona <70548141+alyonafuria@users.noreply.github.com>
2022-02-24 03:48:26 +0300

    feat: add solution to brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    const BRACKETS_PAIR = {};
+    for (i = 0; i < bracketsConfig.length; i++) {
+        BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+    
+    const OPEN_BRACKETS = Object.values(BRACKETS_PAIR);
+
+    const SAME_SYMBOLS = [];
+    for (i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+          SAME_SYMBOLS.push(bracketsConfig[i][0]);
+        }
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      if (OPEN_BRACKETS.includes(currentSymbol) && !SAME_SYMBOLS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else if (SAME_SYMBOLS.includes(currentSymbol) && !stack.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
+  }
